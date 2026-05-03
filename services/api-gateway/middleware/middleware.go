ccc9483b9ce2eed3152637c3c0fecd271dc4957f
Ayush Devani <ayushdevani01@gmail.com>
2026-05-03 11:32:30 +0530

    allow api key headers in cors preflight

--- services/api-gateway/middleware/middleware.go
+++ services/api-gateway/middleware/middleware.go
@@ -109,7 +109,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key, x-app-id")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
